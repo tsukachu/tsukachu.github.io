855add18591c2a8b12afd1f693cf15a424bc75b2
tsukachu <concon.ch.1543@gmail.com>
2019-12-14 09:48:29 +0900

    Add basic layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+
+import BasicLayout from "@/components/templates/BasicLayout.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: BasicLayout,
+    children: [
+      {
+        path: "",
+        component: Home
+      }
+    ]
   }
 ];
 
