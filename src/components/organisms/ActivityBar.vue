e0f4d71e024f14422bd721905e16af7699d53a52
tsukachu <concon.ch.1543@gmail.com>
2019-12-16 18:22:01 +0900

    Add icon to ActivityBar and Fix layout

--- src/components/organisms/ActivityBar.vue
+++ src/components/organisms/ActivityBar.vue
@@ -1,10 +1,39 @@
 <template>
-  <div class="activity-bar"></div>
+  <div class="activity-bar d-flex flex-column">
+    <ul class="nav mt-auto">
+      <li>
+        <div class="icon d-flex align-items-center justify-content-center">
+          <a
+            href="https://github.com/tsukachu/tsukachu.github.io"
+            target="_blank"
+          >
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </a>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss">
 .activity-bar {
   background-color: rgb(51, 51, 51);
   width: 48px;
+}
+
+.icon {
+  height: 48px;
+  width: 48px;
+
+  margin-bottom: 4px;
+
+  svg {
+    font-size: 24px;
+    color: rgba(255, 255, 255, 0.4);
+
+    &:hover {
+      color: rgb(255, 255, 255);
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faGithub);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
