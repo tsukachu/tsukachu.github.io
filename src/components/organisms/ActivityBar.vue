95e23717694779da7dfe22bfc79cc2bdc82aa60f
tsukachu <concon.ch.1543@gmail.com>
2019-12-16 20:11:36 +0900

    ActivityBar elements to component

--- src/components/organisms/ActivityBar.vue
+++ src/components/organisms/ActivityBar.vue
@@ -1,39 +1,35 @@
 <template>
   <div class="activity-bar d-flex flex-column">
-    <ul class="nav mt-auto">
-      <li>
-        <div class="icon d-flex align-items-center justify-content-center">
-          <a
-            href="https://github.com/tsukachu/tsukachu.github.io"
-            target="_blank"
-          >
-            <font-awesome-icon :icon="['fab', 'github']" />
-          </a>
-        </div>
-      </li>
-    </ul>
+    <VerticalList class="mt-auto" :links="links" />
   </div>
 </template>
+
+<script>
+import VerticalList from "@/components/molecules/VerticalList.vue";
+
+export default {
+  name: "activity-bar",
+  components: {
+    VerticalList
+  },
+  data: function() {
+    return {
+      links: [
+        {
+          to: "https://github.com/tsukachu/tsukachu.github.io",
+          faPrefix: "fab",
+          faName: "github",
+          isExternal: true
+        }
+      ]
+    };
+  }
+};
+</script>
 
 <style lang="scss">
 .activity-bar {
   background-color: rgb(51, 51, 51);
   width: 48px;
-}
-
-.icon {
-  height: 48px;
-  width: 48px;
-
-  margin-bottom: 4px;
-
-  svg {
-    font-size: 24px;
-    color: rgba(255, 255, 255, 0.4);
-
-    &:hover {
-      color: rgb(255, 255, 255);
-    }
-  }
 }
 </style>
